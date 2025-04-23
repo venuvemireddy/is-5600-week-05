82a0f4a1907986724613af2e7eb699bf6b510db2
venuvemireddy <venugopalreddy.vemireddy@slu.edu>
2025-04-23 03:45:24 +0000

    Lab-5

--- api.js
+++ api.js
@@ -44,35 +44,62 @@ async function getProduct(req, res, next) {
   return res.json(product)
 }
 
-/**
- * Create a product
- * @param {object} req 
- * @param {object} res 
- */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
- * Edit a product
- * @param {object} req
- * @param {object} res
- * @param {function} next
+ * Update a product
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
-async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
 }
 
 /**
  * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
-async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+
+/**
+ * Create an order
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(orders)
+}
+
+/**
+ * List orders
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
 }
 
 module.exports = autoCatch({
@@ -81,5 +108,7 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  listOrders,
+  createOrder
 });
